10c66d339cf13c6c695ffb433e35f6c313cd5a40
geekalaa <alaa.kaddour1@esprit.tn>
2021-10-22 21:27:41 +0100

    adding project

---
+++ src/main/java/com/services/groupf/Group.java
@@ -0,0 +1,52 @@
+package com.services.groupf;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "group_table")
+public class Group implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String title;
+	private String description;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Group() {
+		super();
+	}
+	public Group(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+
+	
+	
+
+}
